5d00e57fad5c10be4bbb31a758a5ae4b64251496
curioe <curioe@gmail.com>
2013-02-27 21:05:50 +0900

    타임존 KST로 변경

--- dateutil.py
+++ dateutil.py
@@ -1,18 +1,19 @@
 import datetime
 
-class Eastern_tzinfo(datetime.tzinfo):
-    """Implementation of the Eastern timezone.
+class Seoul_tzinfo(datetime.tzinfo):
+    """Implementation of the Seoul timezone.
     
     Adapted from http://code.google.com/appengine/docs/python/datastore/typesandpropertyclasses.html
     """
     def utcoffset(self, dt):
-        return datetime.timedelta(hours=-5) + self.dst(dt)
+        return datetime.timedelta(hours=+9) + self.dst(dt)
 
     def _FirstSunday(self, dt):
         """First Sunday on or after dt."""
         return dt + datetime.timedelta(days=(6-dt.weekday()))
 
     def dst(self, dt):
+        """ 한국은 섬머타임 없으므로 계산 생략 (dst=daylight saving time)
         # 2 am on the second Sunday in March
         dst_start = self._FirstSunday(datetime.datetime(dt.year, 3, 8, 2))
         # 1 am on the first Sunday in November
@@ -22,17 +23,21 @@ class Eastern_tzinfo(datetime.tzinfo):
             return datetime.timedelta(hours=1)
         else:
             return datetime.timedelta(hours=0)
-        
+        """
+        return datetime.timedelta(hours=0)
+
     def tzname(self, dt):
+        """
         if self.dst(dt) == datetime.timedelta(hours=0):
-            return "EST"
+            return "KST"
         else:
-            return "EDT"
-        
+            return "KDT"
+        """
+        return "KST"
         
 def date_for_new_snippet():
     """Return next Monday, unless it is Monday (0) or Tuesday (1)"""
-    today = datetime.datetime.now(Eastern_tzinfo()).date()
+    today = datetime.datetime.now(Seoul_tzinfo()).date()
     if (today.weekday() < 2):
         aligned = today - datetime.timedelta(days=today.weekday())
     else:
@@ -42,5 +47,5 @@ def date_for_new_snippet():
 
 def date_for_retrieval():
     """Always return the most recent Monday."""
-    today = datetime.datetime.now(Eastern_tzinfo()).date()
+    today = datetime.datetime.now(Seoul_tzinfo()).date()
     return today - datetime.timedelta(days=today.weekday())
